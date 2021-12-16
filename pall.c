766e642c2dcea78a2f9dd7948ec2163ef228aed8
Adam Clear <3589@holbertonschool.com>
2021-12-16 21:38:43 +0000

    added new files

---
+++ pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * pall - prints all the values in the stack
+ * @stack: Pointer to the stack
+ * @line_count: Line number from main
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
+{
+	stack_t **current;
+
+	if (stack)
+	{
+		current = stack;
+		while (*current)
+		{
+			printf("%d\n", (*current)->n);
+			(*current) = (*current)->prev;
+		}
+	}
+return;
+}
---
+++ push.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+ * push - adds a node to the stack
+ * @stack: Pointer to the stack
+ * @lncnt: The line count from main
+ * @arg: Argument passed with function
+ */
+void push(stack_t **stack, unsigned int lncnt)
+{
+	stack_t *newnode = malloc(sizeof(stack_t));
+
+	if (!newnode)
+	{
+		free(newnode);
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	if ((isdigit(arg)) == 1)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", lncnt);
+		exit(EXIT_FAILURE);
+	}
+	newnode->n = arg;
+	newnode->prev = *stack;
+	*stack = newnode;
+	free(newnode);
+return;
+}
