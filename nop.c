274e8488829b8230c701914a0cc46d78c65b213c
AdamNB-sys <a1adnat@gmail.com>
2021-12-17 10:52:45 -0800

    nop function, the easiest one

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - no operation, does nothing. NOTHING AT ALL
+ * @stack: the stack
+ * @line_number: line number
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
